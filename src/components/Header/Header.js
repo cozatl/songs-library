fd04d4ba514b8efcbc09312274e34ffcb266041d
jesus cozatl <cozatlram@yahoo.com.mx>
2026-01-21 17:43:53 -0600

    improvements in the components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,19 +4,20 @@ class Header extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isloggedIn: false,
             welcomeMessage: 'Welcome'
         }
     }
 
+    componentDidMount() {
+        console.log('Web Page loaded correctly.')
+    }
+    
     render() {
         return (
             <header>
                 <span></span>
                 <h1>{this.props.appName}</h1>
-                {/* <button onClick={this.handleButtonClick}>Click Me</button>
-                <p>{this.state.welcomeMessage}</p>
-                <p>{this.state.isloggedIn ? 'Welcome, user' : 'You are not connected'}</p> */}
+                {/* <p>{this.state.welcomeMessage}</p> */}
             </header>
         )
     }
